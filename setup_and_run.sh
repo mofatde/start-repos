cd8ff6c7b38121ace2d2de164f951ee70a969501
mf <mofa.tde@gmail.com>
2024-09-30 13:36:39 +0000

    export v1

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -11,6 +11,8 @@ REPO_URL=$1
 # Extract repo name from URL
 REPO_NAME=$(basename -s .git "$REPO_URL")
 
+rm -rf "$REPO_NAME"
+
 # Step 1: Clone the repository
 echo "Cloning repository..."
 git clone "$REPO_URL"
@@ -24,6 +26,7 @@ fi
 # Step 2: Navigate into the repository directory
 cd "$REPO_NAME" || { echo "Failed to enter directory"; exit 1; }
 
+echo $(ls)
 # Step 3: Create a virtual environment
 echo "Creating virtual environment..."
 python3 -m venv env
@@ -33,16 +36,17 @@ echo "Activating virtual environment..."
 source env/bin/activate
 
 # Step 5: Install dependencies (if there's a requirements.txt or environment.yml)
-if [ -f "requirements.txt" ]; then
+if [ -f "env_data/requirements.txt" ]; then
     echo "Installing dependencies..."
-    pip install -r requirements.txt
-elif [ -f "environment.yml" ]; then
+    pip install -r env_data/requirements.txt
+elif [ -f "env_data/environment.yml" ]; then
     echo "Installing dependencies from environment.yml..."
     conda env create -f environment.yml
 else
     echo "No requirements.txt or environment.yml found. Skipping dependency installation."
 fi
 
+#exit  21
 # Step 6: Run the script (assuming script runner.py is in the repo)
 echo "Running the script..."
 python env_data/run_script.py 
